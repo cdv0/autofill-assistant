a084fbc43cfe5a7c533ddb41d191301a853e05e0
Cathleen Vu <155689506+cdv0@users.noreply.github.com>
2026-03-27 02:35:44 -0700

    Create group edit organism

--- src/components/organisms/GroupEdit.tsx
+++ src/components/organisms/GroupEdit.tsx
@@ -1,9 +1,12 @@
 import Button from "../atoms/Button";
-import LabelView from "../molecules/LabelView";
+import LabelInput from "../molecules/LabelInput";
+import { Plus } from "lucide-react";
 
-interface GroupViewProps {
+interface GroupEditProps {
   groupName: string;
   onClickBack: () => void;
+  onClickAddField: () => void;
+  onClickTrash: () => void;
 }
 
 // TODO: Delete dummy data (fields)
@@ -15,25 +18,31 @@ const fields = [
   { id: 5, label: "First name", value: "Cathleen" },
   { id: 6, label: "Last name", value: "Vu" },
   { id: 7, label: "First name", value: "Cathleen" },
-  { id: 8, label: "First name", value: "Cathleen" },
+
 ];
 
-const GroupView = ({ groupName, onClickBack }: GroupViewProps) => {
+const GroupEdit = ({ groupName, onClickBack, onClickAddField, onClickTrash }: GroupEditProps) => {
   return (
     <div className="flex flex-col gap-6">
+      {/* TOP BAR */}
       <div className="flex justify-between">
         <h1 className="font-semibold text-lg text-darkGray">Groups {">"} <span className="text-primary">{groupName}</span></h1>
         <Button color="secondaryNoFill" name="Back" onClick={onClickBack}/>
       </div>
 
-      {/* COLUMNS */}
+      {/* GRID COLUMNS */}
       <div className="grid grid-cols-2 gap-x-12 gap-y-8">
         {fields.map((field) => (
-          <LabelView key={field.id} label={field.label} value={field.value}/>
+          <LabelInput key={field.id} labelText={field.label} placeholder="Type here" type="text" addTrashIcon={true} onClick={onClickTrash} />
         ))}
+
+        {/* ADD NEW FIELD BUTTON */}
+        <div className="pt-6">
+          <Button color="darkGrayNoFill" name="Add new field" icon={Plus} onClick={onClickAddField}/>
+        </div>
       </div>
     </div>
   );
 };
 
-export default GroupView;
+export default GroupEdit;
