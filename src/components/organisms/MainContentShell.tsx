ce6c95bc4ab6703194613c333607002923a79a1c
Cathleen Vu <155689506+cdv0@users.noreply.github.com>
2026-03-27 02:31:24 -0700

    Update style

--- src/components/organisms/MainContentShell.tsx
+++ src/components/organisms/MainContentShell.tsx
@@ -1,14 +1,13 @@
-import { Group } from "lucide-react";
-import Groups, { type GroupsProps } from "./Groups";
-import GroupView from "./GroupView";
+import { type GroupsProps } from "./Groups";
+import GroupEdit from "./GroupEdit";
 
 interface ShellProps extends GroupsProps {
 }
 
 const Shell = (props: ShellProps) => {
   return (
-    <div className="bg-white py-4 px-6 border border-stroke rounded-2xl">
-      <GroupView />
+    <div className="bg-white py-4 px-6 pb-8 border border-stroke rounded-2xl">
+      <GroupEdit groupName="Health" onClickBack={() => {}} />
     </div>
   )
 }
