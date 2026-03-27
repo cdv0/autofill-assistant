f38c62eda14918bb6466e4ac9d2a8b1e98f88206
Cathleen Vu <155689506+cdv0@users.noreply.github.com>
2026-03-27 14:33:23 -0700

    Update navigation item

--- src/components/atoms/NavItem.tsx
+++ src/components/atoms/NavItem.tsx
@@ -3,7 +3,8 @@ import type { LucideIcon } from "lucide-react"
 
 const styleClasses = {
     lightBlue: {hover: "hover:bg-lightBlue", text: "text-primary"},
-    danger: {hover: "hover:bg-red-100", text: "hover:text-danger"}
+    danger: {hover: "hover:bg-red-100", text: "hover:text-danger"},
+    lightGray: {hover: "hover:bg-gray-100", text: "text-primary"},
 }
 
 interface NavProps {
@@ -12,6 +13,8 @@ interface NavProps {
     isDropdown?: boolean;
     icon?: LucideIcon;
     style?: keyof typeof styleClasses;
+    className?: string;
+    isOpen?: boolean;
 }
 
 const NavItem = ({
@@ -20,14 +23,16 @@ const NavItem = ({
     isDropdown = false,
     icon: Icon,
     style = "lightBlue",
+    className,
+    isOpen,
 }: NavProps) => {
     return (
-        <button className={`flex justify-between items-center py-2 px-4 w-full rounded-xl ${styleClasses[style].hover} ${styleClasses[style].text} transition`} onClick={onClick}>
+        <button className={`flex justify-between items-center py-2 px-4 w-full rounded-xl ${styleClasses[style].hover} ${styleClasses[style].text} transition ${className}`} onClick={onClick}>
             <div className="flex justify-center items-center gap-2">
                 {Icon && <Icon className="w-4 h-4" />}
                 <p className={`font-semibold`}>{text}</p>
             </div>
-            {isDropdown && <ChevronDown className="w-4 h-4"/>}
+            {isDropdown && <ChevronDown className={`w-4 h-4 ${isOpen? "rotate-180" : "rotate-0"}`}/>}
         </button>
     )
 }
