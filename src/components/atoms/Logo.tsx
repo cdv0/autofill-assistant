62aa0fbaabed7c99e189af0d87b796d0f5ddfdba
Cathleen Vu <cathleendvu@gmail.com>
2026-04-11 23:04:07 -0700

    Fix logo image

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,3 +1,5 @@
+import logo from "../../assets/logo-image.svg";
+
 const sizeClasses = {
     sm: { img: "h-10 w-10", text: "text-lg"},
     md: { img: "h-12 w-12", text: "text-xl"},
@@ -19,7 +21,7 @@ const Logo = ({
 }: LogoProps) => {
     return (
         <button onClick={onClickLogo} className="flex justify-center items-center gap-2 p-2 cursor-pointer">
-            <img src="../src/assets/logo-image.svg" className={sizeClasses[size].img} />
+            <img src={logo} className={sizeClasses[size].img} />
             {!iconOnly && text && <p className={`font-istok font-bold ${sizeClasses[size].text}`}>Auto Fill AI</p>}
         </button>
     )
